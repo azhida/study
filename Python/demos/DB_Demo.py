9dd071c5bf2a08f561848087c95480062b8cf353
azhida <334064034@qq.com>
2021-11-12 18:28:16 +0800

    improve DB_Demo.py

--- Python/demos/DB_Demo.py
+++ Python/demos/DB_Demo.py
@@ -3,6 +3,8 @@ import pymysql
 import time
 import sys
 import os
+import logging
+import inspect
 
 # 先安装扩展库 pandas pymysql
 # pip install pandas pymysql
@@ -10,6 +12,9 @@ import os
 # 调用示例：
 # python DB_Demo.py MySQL主机地址 用户名 用户密码 数据库名称 数据表名称 动作名称 csv文件绝对路径
 # python DB_Demo.py 127.0.0.1 root root db_test table_test action_name csv_file_path
+
+# 使用日志模块要确保存在 logs 目录，否则报错
+logging.basicConfig(filename='./logs/db.log')
 
 
 class DB:
@@ -39,29 +44,51 @@ class DB:
         db.cur.close()
         db.con.close()
 
+    def log(self, message, log_file_name='./logs/db.log'):
+        # 获取调用者的方法名
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 2)
+        caller_name = calframe[1][3]
+        logging.basicConfig(filename=log_file_name)
+        logging.log(logging.ERROR, f'{self.datetime()} , {__class__.__name__}::{caller_name}() => {message}')
+
+    def datetime(self):
+        # 时间日期格式 2000-01-01 00:00:00
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    # 返回 调用者的方法名
+    def get_function_name(self):
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 2)
+        caller_name = calframe[1][3]
+        # print('caller name:', caller_name)
+        return caller_name
+
     def csv_to_mysql(self):
 
         try:
-            print(self.table_name, '执行开始')
             start_time_1 = time.time()
+
+            self.log(f'{self.table_name} 执行开始 count : 0')
+
             chunksize = 1000
             count = 1
             for df in pd.read_csv(self.file_path, chunksize=chunksize):
-                # print('执行开始 count : ', count, df)
-                # print(11, tuple(df.columns))
-
-                print(self.table_name, '执行开始 count : ', count)
-                count += 1
 
                 start_time_2 = time.time()
+                self.log(f'{self.table_name} 执行开始 count : {count}')
+                self.log(f'tuple(df.columns) : {tuple(df.columns)}')
+
+                count += 1
 
                 try:
 
                     # 数据表字段
                     select_colunm = str(tuple(df.columns))
-                    # print(2, select_colunm, type(select_colunm))
+                    self.log(f'1 type(select_colunm) : {type(select_colunm)} ; select_colunm : {select_colunm} ; ')
+
                     select_colunm = select_colunm.replace("'", '`')
-                    # print(2, select_colunm, type(select_colunm))
+                    self.log(f'2 type(select_colunm) : {type(select_colunm)} ; select_colunm : {select_colunm} ; ')
 
                     # 将 表中所有为 null的数据 标记为 None_Null
                     df = df.astype(object).where(pd.notnull(df), 'None_Null')
@@ -81,31 +108,34 @@ class DB:
 
                     # 拼接sql
                     insert_sql = 'insert into `{}` {} values {}'.format(self.table_name, select_colunm, insert_data)
-                    # print(90, insert_sql)
+                    self.log(f'insert_sql : {insert_sql} ;')
 
                     self.cur.execute(insert_sql)
                     self.con.commit()
 
                     end_time_2 = time.time()
                     used_time_2 = end_time_2 - start_time_2
-                    print(self.table_name, '执行结束, used_time_2 : %s' % used_time_2)
+                    self.log(f'{self.table_name} 执行结束，used_time_2 = {used_time_2} ;')
 
                 except Exception as e:
+                    self.log(f'抛出异常：{e}')
                     exit(e)
 
             end_time_1 = time.time()
             used_time_1 = end_time_1 - start_time_1
             print(self.table_name, '执行结束 used_time_1 = ', used_time_1)
+            self.log(f'{self.table_name} 执行结束，used_time_1 = {used_time_1}')
 
         except Exception as e:
+            self.log(f'抛出异常：{e}')
             exit(e)
 
     def mysql_to_csv(self):
 
         try:
-
-            print(self.table_name, '执行开始')
             start_time_1 = time.time()
+
+            self.log(f'{self.table_name} 执行开始')
 
             # print(os.getcwd()) # 获取当前工作目录路径
             # print(os.path.abspath('.')) # 获取当前工作目录路径
@@ -113,38 +143,41 @@ class DB:
             # 当前文件所在的目录
             current_file_dir = os.path.dirname(__file__)
             print('current_file_dir', current_file_dir)
+            self.log(f'current_file_dir : {current_file_dir} ')
 
             # 年月日
             datetime_str = time.strftime("%Y%m%d%H%M%S", time.localtime())
-            print(datetime_str)
+            self.log(f'datetime_str : {datetime_str} ')
 
             # 文件保存路径[动态生成绝对路径]
             save_path = os.path.join(current_file_dir, 'csvs/' + datetime_str)
-            print('save_path', save_path)
+            self.log(f'save_path : {save_path} ')
 
             # 判断文件夹是否存在，不存在则创建文件夹
             if not os.path.exists(save_path):
                 os.makedirs(save_path)
 
             sql = 'select * from %s' % self.table_name
-            print('sql', sql)
+            self.log(f'sql : {sql} ')
 
             # 参数encoding="utf_8_sig"编码后，可以防止写入csv的中文出现乱码
             df = pd.read_sql(sql, self.con)
-            print('df.head(2)', df.head(2))
+            self.log(f'df.head(2) : {df.head(2)} ')
 
             df.to_csv(save_path + '/%s.csv' % self.table_name, index=None, encoding="utf_8_sig")
 
             end_time_1 = time.time()
             used_time_1 = end_time_1 - start_time_1
             print(self.table_name, '执行结束 used_time_1 = ', used_time_1)
+            self.log(f'{self.table_name} 执行结束，used_time_1 = {used_time_1}')
 
         except Exception as e:
             exit(e)
 
 
 # 接收命令行参数
-print(f'命令行参数：{sys.argv}')
+logging.log(logging.ERROR, f'\r命令行参数：{sys.argv}')
+
 host = sys.argv[1]
 user = sys.argv[2]
 password = sys.argv[3]

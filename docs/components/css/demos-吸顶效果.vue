0929405799281600170abf3b2e66f26e36b738e6
wgh <334064034@qq.com>
2023-05-22 07:49:14 +0800

    add:docs/web/css/demos/吸顶效果.md

---
+++ docs/components/css/demos-吸顶效果.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="main" >
+
+        <div class="box">
+            <div class="header">
+            header
+        </div>
+        
+        <div class="ding">
+            ding
+        </div>
+
+        <div class="content">
+            content
+        </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import * as _ from 'lodash-es'
+
+export default{
+    data(){
+        return {
+           
+        }
+    },
+    methods:{
+       
+    }
+}
+</script>
+
+<style scoped>
+
+    .main{
+        height: 500px;
+        border: 1px solid red;
+        overflow: scroll;
+        
+    }
+
+    .box{
+
+        
+
+    }
+
+    .header{
+        height: 100px;
+        background-color: gray;
+    }
+
+    .ding{
+        height: 100px;
+        background-color: red;
+        position: sticky;
+        top: 0;
+    }
+
+    .content{
+        height: 800px;
+        background-color: blue;
+    }
+
+    
+</style>

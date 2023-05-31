32a9c744145f50649f9de10bd9c042e3b10df5cc
wgh <790409257@qq.com>
2023-05-31 19:21:41 +0800

    add:css/demos/显示两行文字多余省略

---
+++ docs/components/css/demos-显示两行文字多余省略.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="main">
+		文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+		文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+		文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+		文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+	</div>
+</template>
+
+<style scoped>
+	.main {
+		width: 100%;
+		border: 1px solid red;
+
+		text-overflow: ellipsis;
+		overflow: hidden;
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+		-webkit-box-orient: vertical;
+	}
+</style>

d1da0e9efc53769188a1f10d489c10b0e199f804
wgh <790409257@qq.com>
2023-08-17 12:13:55 +0800

    vue/h5扫码/QRScan-vue3-qr-reader.vue

---
+++ docs/web/vue/h5扫码/QRScan-vue3-qr-reader.vue
@@ -0,0 +1,47 @@
+<template>
+    <qr-stream class="qr-stream-box" @decode="onDecode" style="">
+        <div class="frame"></div>
+        <van-icon class="close" name="clear" color="#f7f8fa" size="8vw" @click="onClickClose" />
+    </qr-stream>
+</template>
+
+<script setup lang="ts">
+/**
+ * @function success
+ * @function error
+ * @function close
+ */
+import { QrStream, QrCapture, QrDropzone } from 'vue3-qr-reader';
+
+const emit = defineEmits(['success', 'error', 'close'])
+
+function onDecode(data) {
+    emit('success', data)
+}
+
+function onClickClose() {
+    emit('close', false)
+}
+</script>
+
+<style lang="scss" scoped>
+.qr-stream-box{
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    .frame{
+        color: red;
+    }
+
+    .close {
+        position: absolute;
+        top: 20px; 
+        right: 20px;
+    }
+}
+</style>
+
+
--- docs/web/vue/h5扫码/index.md
+++ docs/web/vue/h5扫码/index.md
@@ -1,7 +1,35 @@
 # H5 扫码 
 
+
+::: danger 注意 
+H5 调手机摄像头需要在 https 环境下使用，否则将不保证可以正常工作！
+:::
+
 ## html5-qrcode
 
 - 官网
 
 https://scanapp.org/html5-qrcode-docs/
+
+## vue-qrcode-reader
+
+- 官网
+
+https://vue-qrcode-reader.netlify.app/
+
+## vue3-qr-reader
+
+- 官网
+
+https://vue-qrcode-reader.netlify.app/
+
+::: details 示例代码
+/QRScan.vue
+
+<<< @/web/vue/h5扫码/QRScan-vue3-qr-reader.vue
+
+```vue
+<QRScan v-if="show" @success="onSuccess" @close="show = false" ></QRScan>
+```
+:::
+

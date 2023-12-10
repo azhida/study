2bba2f439d3956ab36a6c6fa3865c9f5c6eef116
wgh <334064034@qq.com>
2023-12-10 21:57:25 +0800

    grid网格布局-等宽自适应

---
+++ docs/web/css/demos/grid网格布局-等宽自适应.md
@@ -0,0 +1,16 @@
+# Grid 布局 - 等宽自适应
+
+## 效果展示
+
+
+<CssDemo />
+<script setup>
+import CssDemo from './grid网格布局等宽自适应.vue'
+</script>
+
+
+## 示例代码
+
+
+<<< @/web/css/demos/grid网格布局等宽自适应.vue
+
---
+++ docs/web/css/demos/grid网格布局等宽自适应.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="main">
+        <div class="item" v-for="i in 10">item</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+	.main {
+		width: 100%;
+		border: 1px solid red;
+		display: grid;
+		// 每个元素宽度：最小 100px，最大 占满
+		grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+
+        .item {
+            height: 50px;
+			border: 1px solid blue;
+        }
+	}
+</style>

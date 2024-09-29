72c0de239fb03c7c3dc4ca8f8227a07a48dee3a7
wgh <790409257@qq.com>
2024-09-29 16:29:57 +0800

    add: css/动画/流动边框-组件

---
+++ apps/web/src/css/动画/流动边框-组件.vue
@@ -0,0 +1,60 @@
+<!-- ./流动边框-demo-1.vue -->
+
+<template>
+  <div class="box">
+    <div class="transform-box">
+      <div class="animation-box"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+
+  .transform-box {
+    height: 100%;
+    width: 100%;
+    transform-origin: 50% 50%;
+    transform: scale(10);
+
+    .animation-box {
+      position: absolute;
+      border: 1px solid red;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      overflow: hidden;
+      transform: translate(-50%, -50%);
+      top: 50%;
+      left: 50%;
+      transform-origin: 50% 50%;
+
+      &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        // display: block;
+        border-radius: 50%;
+        background: orange;
+        clip-path: polygon(50% 50%, 100% 50%, 100% 23%, 100% 0, 74% 0, 53% 0, 15% 0);
+        animation: rotate 5s linear infinite;
+      }
+
+    }
+
+  }
+}
+
+@keyframes rotate {
+  to {
+    transform: rotate(1turn);
+  }
+}
+</style>
--- apps/web/src/css/动画/流动边框.vue
+++ apps/web/src/css/动画/流动边框.vue
@@ -3,11 +3,26 @@
   <div class="main">
     <demo1></demo1>
     <demo2></demo2>
+
+    <div class="demo3">
+      <demo style=""></demo>
+    </div>
   </div>
 </template>
 
 <script setup>
+import demo from './流动边框-组件.vue'
 import demo1 from './流动边框-demo-1.vue'
 import demo2 from './流动边框-demo-2.vue'
 
 </script>
+
+<style lang="scss" scoped>
+.demo3{
+  position: relative;
+  width: 200px;
+  height: 100px;
+  border: 1px solid blue;
+  background-color: red;
+}
+</style>

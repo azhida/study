1d145854f08ce6d93fdcbd4e2007d9d5f38741ac
wgh <790409257@qq.com>
2025-04-30 08:37:50 +0800

    add:自定义导航栏组件项

---
+++ packages/components/CustomNavItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <a class="VPNavBarMenuLink" :class="{'active': route.path == link}" :href="link">{{ text }}</a>
+</template>
+
+<script setup>
+import { useRoute } from 'vitepress'
+
+const {text,link} = defineProps(['text','link'])
+const route = useRoute()
+</script>
+
+
+<style scoped lang="scss">
+  .VPNavBarMenuLink {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    line-height: var(--vp-nav-height);
+    font-size: 14px;
+    font-weight: 500;
+    color: var(--vp-c-text-1);
+    transition: color 0.25s;
+    cursor: pointer;
+
+    &.active {
+      color: var(--vp-c-brand-1);
+    }
+  }
+
+</style>
---
+++ packages/components/nav.js
@@ -0,0 +1,14 @@
+export function getNavs() {
+  const navs = [
+    { text: 'Home', link: '/' },
+    { text: '前端', link: '/web/' },
+    { text: '服务端', link: '/web/' },
+    { text: '数据库', link: '/web/' },
+    { text: '开发工具', link: '/web/' },
+    { text: '项目案例', link: '/web/' },
+    { text: '其他', link: '/web/' },
+    { text: 'AI', link: '/ai/' },
+  ]
+  return navs;
+}
+

47b494e03d7fe45324a9fbfb13e3d9e12d2b1151
wgh <790409257@qq.com>
2025-05-06 16:27:16 +0800

    up: apps.other.配置打包路径

--- apps/other/.envs/cf.ts
+++ apps/other/.envs/cf.ts
@@ -1,10 +1,11 @@
 // 部署到 cloudflare page
-
+import path from "path";
 import { UserConfig } from "vitepress"
 
-export const config:UserConfig = {
+export const config: UserConfig = {
   base: '/other/',
-  outDir: '../../cf-pages/other',
+  // outDir: '../../cf-pages/other',
+  outDir: path.resolve(__dirname, '../../../cf-pages/other'),
 }
 
 export {

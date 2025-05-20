ac1c84c9845e814ec03910897698610603fd5af2
wgh <790409257@qq.com>
2025-05-20 12:19:25 +0800

    add: apps.web.css.操作CSS变量

---
+++ apps/web/src/css/demos/操作CSS变量.md
@@ -0,0 +1,15 @@
+# 操作 CSS 变量
+
+## 效果展示
+
+
+<Demo />
+<script setup>
+import Demo from './操作CSS变量.vue'
+</script>
+
+
+## 示例代码
+
+<<< ./操作CSS变量.vue
+
---
+++ apps/web/src/css/demos/操作CSS变量.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :style="{'--bg-color': 'red', '--border-color': 'blue'}">
+    <div class="demo-box">
+      123
+    </div>
+  </div>
+  
+</template>
+<style scoped>
+.demo-box {  
+  width: 200px;
+  height: 200px;
+  background-color: var(--bg-color);
+  border: 2px solid var(--border-color);
+}
+</style>
